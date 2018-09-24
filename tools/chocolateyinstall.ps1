925024f1387ef761c938231f45e76de1b9be1de6
Ferdinand Silva <ferdinandsilva@ferdinandsilva.com>
2018-09-24 19:05:12 +0800

    first commit

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://dl.bintray.com/six519/Generic/setup_2.0.0.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+
+  softwareName  = 'titik*' 
+
+  checksum      = '5e80c182b1518a9a7e225def0dd65a72a05fe1cb5b95db6cca39f11587f464c4'
+  checksumType  = 'sha256'
+  silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' 
+}
+
+Install-ChocolateyPackage @packageArgs
